408a33d24ea9e0eec6b741555b1ce9cc0d43c22c
danx <rushn24@gmail.com>
2024-09-27 21:53:26 -0400

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>I am Counter</h1>
+            </div>
+        );
+    };
+};
+
+export default Counter;
