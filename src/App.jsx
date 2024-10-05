df3f1e48b7957a2621ceeff5861596ba557a5c32
danx <rushn24@gmail.com>
2024-10-04 20:02:21 -0400

    added class component Counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './Counter'
 
 function App() {
 
   return (
     <>
-      
+      <Counter color="purple" initialVal={10} />  
     </>
   )
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 class Counter extends React.Component {
+    constructor(props){
+        super(props)
+        console.log(this.props)
+    }
     render() {
+        const { color } = this.props
         return (
             <div>
-                <h1>I am Counter</h1>
+                <h1 style={{color}}>I am Counter</h1>
             </div>
         );
     };
