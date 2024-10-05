14380309c754c6ef238d84c5d7f0bfa1f0404067
danx <rushn24@gmail.com>
2024-10-04 20:48:16 -0400

    added some component methods

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,13 +3,34 @@ import React from 'react';
 class Counter extends React.Component {
     constructor(props){
         super(props)
-        console.log(this.props)
+        this.state = { 
+            count: 0, 
+            isHiding: false 
+        };
+        this.decrement = this.decrement.bind(this);
+    }
+    decrement() {
+        this.setState({ count: this.state.count - 1 })    
+    }
+    componentDidMount(){
+        console.log("Component mounted")
+    }
+    componentDidUpdate(){
+        console.log("Updated")    
+    }
+    componentWillUnmount(){
+        console.log("About to go bye-bye")
     }
     render() {
-        const { color } = this.props
+        console.log("Component rendering")
+        const { color } = this.props;
+        const { count } = this.state;
         return (
             <div>
                 <h1 style={{color}}>I am Counter</h1>
+                <h3>Count is: {count} </h3>
+                <button onClick={() => this.setState({ count: count + 1 })}>Add 1</button>
+                <button onClick={this.decrement}>Subtract 1</button>
             </div>
         );
     };
