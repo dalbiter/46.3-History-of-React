31815fed64fc82e76bf737a2322e69a59eed53fc
danx <rushn24@gmail.com>
2024-10-31 22:26:57 -0400

    added higher order components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import CounterHOC from './HOC/CounterUsingHOC'
 
 function App() {
 
   return (
     <>
       <Counter color="purple" initialVal={10} />  
+      <Counter initialVal={10} /> 
+      <CounterHOC /> 
     </>
   )
 }
---
+++ src/HOC/withCounter.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const withCounter = Component => {
+    return class extends React.Component {
+        state = {
+            count: 0
+        };
+    
+        handleDecrement = () => {
+            this.setState({ count: this.setState.count - 1 });
+        };
+    
+        handleIncrement = () => {
+            this.setState({ count: this.setState.count + 1 });
+        };
+    
+        render() {
+            return (
+                <Component
+                    {...this.props}
+                    count={this.state.count}
+                    onIncrease={this.handleIncrement}
+                    onDecrease={this.handleDecrement} 
+                />
+            );
+        };
+    };
+};
+
+    
+export default withCounter;
